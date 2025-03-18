7a34911a57b6fafef90effbf95c8bdaa5d12a6cb
Ahmed Tarek <ahmed.tarek200288@gmail.com>
2025-03-18 10:14:28 +0200

    add homescreen intro

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Card = ({ size, position, path, alt, style }) => {
+const Card = ({ width, height, position, path, alt, style, marginTop, marginBottom, marginLeft, marginRight }) => {
   return (
-    <div className="card" style={{ ...style, position: position }}>
-      <img src={path} alt={alt} style={{ width: size, height: size }} />
+    <div
+      className="card"
+      style={{
+        ...style,
+        position: position,
+        width: width,
+        height: height,
+        marginTop: marginTop,
+        marginBottom: marginBottom,
+        marginLeft: marginLeft,
+        marginRight: marginRight,
+      }}
+    >
+      <img src={path} alt={alt} style={{ width: '100%', height: '100%', borderRadius: '8px' }} />
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,15 +5,38 @@ import Card from "../components/Card";
 function HomePage() {
     return (
         <div className="home-container">
-            <div>
-                <Card
-                    size="150px"
-                    position="flex-right"
-                    path="src/assets/Logo.png"
-                    alt="Description of image"
-                    style={{ border: '1px solid #ccc', borderRadius: '8px' }}
-                />
+            <Card
+                width="100%"
+                height="450px"
+                position="absolute"
+                path="src/assets/HomeScreenValley.jpg"
+                alt="Description of image"
+                style={{ border: '1px solid ', borderRadius: '8px', zIndex: 0 }}
+                marginTop="0px"
+                marginBottom="20px"
+                marginLeft="0px"
+                marginRight="10px"
+            />
+            <div className="content">
+                <h1>Aiming for a Green Future</h1>
+                <p>Join us in creating sustainable cities where clean air and pure water drive healthier communities. Together, we can build a future that thrives in harmony with nature.</p>
+                <div className="buttons">
+                    <button className="start-button">START</button>
+                    <button className="more-button">FIND OUT MORE</button>
+                </div>
             </div>
+            <Card
+                width="300px"
+                height="400px"
+                position="relative"
+                path="src/assets/GreenCityHomeScreen.jpg"
+                alt="Description of image"
+                style={{ border: '1px solid ', borderRadius: '8px', boxShadow: '0 6px 8px rgba(0, 0, 0)' }}
+                marginTop="170px"
+                marginBottom="20px"
+                marginLeft="10px"
+                marginRight="50px"
+            />
         </div>
     );
 }
