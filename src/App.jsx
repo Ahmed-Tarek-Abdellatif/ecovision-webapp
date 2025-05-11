c1abf271004a2aa508ed4c3a8fcd0d0479b9bb37
Ahmed Tarek <ahmed.tarek200288@gmail.com>
2025-05-11 20:43:25 +0300

    Add Login + code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ import MainLayout from "./Layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import AQI from "./pages/AQI";
 import WQI from "./pages/WQI";
-import LoginPage from "./pages/Login"; // <-- 1. Import LoginPage here
+import LoginPage from "./pages/Login"; // <-- Correct import of LoginPage
+
 
 function App() {
   const router = createBrowserRouter(
@@ -19,7 +20,8 @@ function App() {
         <Route index element={<HomePage />} /> {/* Loads at '/' */}
         <Route path="aqi" element={<AQI />} /> {/* Loads at '/aqi' */}
         <Route path="wqi" element={<WQI />} /> {/* Loads at '/wqi' */}
-        <Route path="login" element={<LoginPage />} /> {/* Loads at '/login' */}
+        <Route path="login" element={<LoginPage />} />
+
       </Route>
     )
   );
